d965fa3d48a904ee1a464b88541be8ab40cb7485
pv <paulovictorflores28@gmail.com>
2022-08-30 16:14:23 -0300

    main mais organizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,10 @@
 import 'package:flutter/material.dart';
 
-//1-a porta de entrada é o main, apartir do main conseguimos start a nossa aplicacao
-void main(List<String> args) {
-//2-aqui dentro chamaremos uma funcao run App e passaremos para ela nosso primeiro widget
+void main(List<String> args) => runApp(PerguntaApp());
 
-//5-vamos passar uma instancia do meu PerguntaApp
-  runApp(new PerguntaApp());
-}
-
-//3- para inserir nosso primeiro widget, temos que criar nosso primeiro widget
-//Entao criaremos uma classe e herdaremos do StatelessWidget nosso primeiro widget
 class PerguntaApp extends StatelessWidget {
-//4-Preciso implementar um metodo build, este metodo retorna um Widget
-
+  @override
   Widget build(BuildContext context) {
-//aqui estou criando uma instacia desse jeito: return new MaterialApp
-    return const MaterialApp(
-      home: Text('Ola mundo!'), //retorno outro widget de texto
-    );
+    return const MaterialApp(home: Text('ola mundo'));
   }
 }
