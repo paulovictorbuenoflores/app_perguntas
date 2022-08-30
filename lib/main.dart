79abd5310c6030e0e3b90d5d1defe2e6c4b71d68
pv <paulovictorflores28@gmail.com>
2022-08-30 16:37:26 -0300

    Criando a estrutura da aplicacao

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,15 @@ void main(List<String> args) => runApp(PerguntaApp());
 class PerguntaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: Text('ola mundo'));
+    return MaterialApp(
+      //scanffold é a estrutura da aplicacao
+      home: Scaffold(
+        //vou comecando a criar minha arvore de componente baseada em composicao
+        appBar: AppBar(
+          title: const Text('Perguntas'),
+        ),
+        body: const Text('Olar flutter'),
+      ),
+    );
   }
 }
